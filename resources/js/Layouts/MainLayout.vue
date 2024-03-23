b4bdb8d30ba2075bbfe8b277ee0daae0adc2fe32
abdulmobeen <mobeendev@gmail.com>
2024-03-23 23:48:24 +0300

    project init

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+    <Link href="/">
+      Home
+    </Link>
+    <Link href="/hello">
+      Hello
+    </Link>
+
+    <slot></slot>
+
+  </template>
+
+  <script setup>
+  import { Link } from '@inertiajs/inertia-vue3'
+  </script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,11 @@
+<template>
+    <MainLayout>
+    <div>Show</div>
+    <Link href="/">Main Page</Link>
+</MainLayout>
+  </template>
+  
+  <script setup>
+  import { Link } from '@inertiajs/inertia-vue3'
+  import MainLayout from '../../Layouts/MainLayout.vue';
+  </script>

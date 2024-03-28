b968716d86b2de449fe113b69353873a45cbf276
abdulmobeen <mobeendev@gmail.com>
2024-03-28 22:49:17 +0300

    added listing form

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,4 @@ use App\Http\Controllers\ListingController;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 Route::resource('listing', ListingController::class)
-  ->only(['index', 'show']);
-  
+  ->only(['index', 'show', 'create', 'store']);

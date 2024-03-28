eaa9ee547fb8460e2f45b556e9ac9bf503dec4e3
abdulmobeen <mobeendev@gmail.com>
2024-03-28 22:42:23 +0300

    added listing controller and views

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,13 @@
+<template>
+    <span>
+      {{ listing.street }} {{ listing.street_nr }}, {{
+        listing.city }}, for ${{ listing.price }}
+    </span>
+  </template>
+  
+  <script setup>
+  defineProps({
+    listing: Object,
+  })
+  </script>
+  
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+Route::resource('listing', ListingController::class)
+  ->only(['index', 'show']);
+  

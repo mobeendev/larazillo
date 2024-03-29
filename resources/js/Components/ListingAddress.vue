2e6208a42dc566448ad03893d093f907bbb04600
abdulmobeen <mobeendev@gmail.com>
2024-03-30 01:34:57 +0300

    updated views

--- resources/js/Components/ListingAddress.vue
+++ resources/js/Components/ListingAddress.vue
@@ -1,13 +1,12 @@
 <template>
-    <span>
-      {{ listing.street }} {{ listing.street_nr }}, {{
-        listing.city }}, for ${{ listing.price }}
-    </span>
-  </template>
-  
-  <script setup>
-  defineProps({
-    listing: Object,
-  })
-  </script>
-  
+  <span>
+    {{ listing.street }} {{ listing.street_nr }}, {{
+      listing.city }}
+  </span>
+</template>
+
+<script setup>
+defineProps({
+  listing: Object,
+})
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -5,7 +5,9 @@
         <Link
           :href="route('listing.show', {listing: listing.id})"
         >
-          <ListingAddress :listing="listing" />
+          <Price :price="listing.price" class="text-2xl font-bold" />
+          <ListingSpace :listing="listing" class="text-lg" />
+          <ListingAddress :listing="listing" class="text-gray-500" />
         </Link>
       </div>
       <div>
@@ -31,6 +33,8 @@
 import {Link} from '@inertiajs/inertia-vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
 import Box from '@/Components/UI/Box.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import Price from '@/Components/Price.vue'
 defineProps({
   listings: Array,
 })

e407c7986fa679e4dc4522f6fcc8c3ff8363d9fc
abdulmobeen <mobeendev@gmail.com>
2024-03-30 14:51:01 +0300

    add inertia progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,15 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import MainLayout from '@/Layouts/MainLayout.vue';
 import { ZiggyVue } from 'ziggy';
+import { InertiaProgress } from '@inertiajs/progress'
 import '../css/app.css'
+
+InertiaProgress.init({
+  delay: 0,
+  color: '#29d',
+  includeCSS: true,
+  showSpinner: true,
+})
 
 createInertiaApp({
   resolve: async (name) => {

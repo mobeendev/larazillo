31f874ed1c37b5c8eef72512417a22a62f14d9db
abdulmobeen <mobeendev@gmail.com>
2024-05-11 12:42:43 +0300

    added images model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
@@ -63,5 +64,10 @@ class Listing extends Model
                 ? $query :
                 $query->orderBy($value, $filters['order'] ?? 'desc')
         );
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ListingImage::class);
     }
 }
